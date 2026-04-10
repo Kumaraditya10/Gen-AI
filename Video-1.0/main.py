97576f1425ccd57dc5557073a62542601fc597e0
Aditya Kumar <aditya10112005@gmail.com>
2026-04-10 19:32:38 +0530

    Implemented multiple AI models across modular components, using main.py as the main entry point to manage and orchestrate application flow.

---
+++ Video-1.0/main.py
@@ -0,0 +1,14 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+responce = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="What is the capital of India?"
+)
+
+print(responce.text)
